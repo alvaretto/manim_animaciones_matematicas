b1ef91b047ba4bd99ff0c37d02d1717b2a398993
alvaretto <alvaroangelm@gmail.com>
2023-08-18 13:39:32 -0500

    Generando Animaciones Matemáticas, con Manim, de Python

---
+++ 01_animar_texto_v01.py
@@ -0,0 +1,70 @@
+from manim import *
+
+class leccion1(Scene):
+    def construct(self):
+        texto1=Text("Hola mundo")
+        self.add(texto1)
+        self.wait(3)
+
+class leccion2(Scene):
+    def contruct(self):
+        texto=Text("Curso de Manim")
+        self.add(texto)
+        self.wait(3)
+        self.remove(texto)
+        self.wait(3)
+
+class leccion3(Scene):
+    def construct(self):
+        texto=Text("Curso de Manim")
+        self.play(FadeIn(texto))
+        self.wait(3)
+        self.play(FadeOut(texto))
+        self.wait(3)
+        self.play(Write(texto))
+        self.wait(3)
+
+class leccion4(Scene):
+    def construct(self):
+        rejilla=NumberPlane()
+        self.add(rejilla)
+        self.wait(5)
+        texto1=Text("Teorema").move_to(RIGHT+UP)
+        texto2 = Text("Lema").move_to(LEFT +DOWN)
+        self.add(texto1, texto2)
+        self.wait(5)
+
+class leccion5(Scene):
+    def construct(self):
+        rejilla=NumberPlane()
+        self.add(rejilla)
+        texto1=Text("A").move_to(RIGHT+UP)
+        texto2=Text("B").move_to(LEFT+DOWN)
+        texto3 = Text("C").move_to(LEFT*2.5)
+        texto4 = Text("D").move_to(DOWN * 3.2)
+        self.add(texto1, texto2, texto3, texto4)
+        self.wait(5)
+
+
+class leccion6(Scene):
+    def construct(self):
+        #rejilla=NumberPlane()
+        #self.add(rejilla)
+        texto1=Text("A").move_to(np.array([1,1,0]))
+        texto2=Text("B").move_to(np.array([-1,-1,0]))
+        texto3 = Text("C").move_to(np.array([-2.5,0,0]))
+        texto4 = Text("D").move_to(np.array([0,-3.2,0]))
+        self.add(texto1, texto2, texto3, texto4)
+        self.wait(5)
+
+class leccion7(Scene):
+    def construct(self):
+        texto1 = Text("Curso de Manim").move_to(UP)
+        texto2 = Text("Curso de Manim").move_to(DOWN)
+        texto3 = Text("primeros pasos").move_to(UP)
+        self.play(FadeIn(texto1))
+        self.wait(3)
+        self.play(Transform(texto1,texto2))
+        self.wait(3)
+        self.play(Transform(texto1, texto3))
+        self.wait(3)        
